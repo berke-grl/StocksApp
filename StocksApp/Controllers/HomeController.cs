0f526eb9a80b43306f534f8310b26d3076f0425f
berke-grl <brkgurel20@hotmail.com>
2023-05-02 16:38:51 +0300

    Add project files.

---
+++ StocksApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StocksApp.Models;
+using StocksApp.Services;
+
+namespace StocksApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly FinnhubService _finnhubService;
+        private readonly IOptions<TradingOptions> _tradingOptions;
+
+        public HomeController(FinnhubService finnhubService, IOptions<TradingOptions> tradingOptions)
+        {
+            _finnhubService = finnhubService;
+            _tradingOptions = tradingOptions;
+        }
+
+
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+            if (_tradingOptions.Value.DefaultStockSymbol == null)
+            {
+                _tradingOptions.Value.DefaultStockSymbol = "MSFT";
+            }
+            Dictionary<string, object>? responseDictinoary = await _finnhubService.GetStockPriceQuote(_tradingOptions.Value.DefaultStockSymbol);
+
+            Stock stock = new Stock()
+            {
+                StockSymbol = _tradingOptions.Value.DefaultStockSymbol,
+                CurrentPrice = Convert.ToDouble(responseDictinoary["c"].ToString()),
+                HighestPrice = Convert.ToDouble(responseDictinoary["h"].ToString()),
+                LowestPrice = Convert.ToDouble(responseDictinoary["l"].ToString()),
+                OpenPrice = Convert.ToDouble(responseDictinoary["o"].ToString()),
+            };
+            return View(stock);
+        }
+    }
+}
---
+++ StocksApp/Program.cs
@@ -0,0 +1,15 @@
+using StocksApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Services
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<FinnhubService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+
+app.Run();
